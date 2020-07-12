d4801c305d34fc87250a6fef785205302a911d0f
John Boyle <nikolasboyle@gmail.com>
2020-07-12 10:22:13 -0700

    Add bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,24 @@
 const router = require("express").Router();
 
 const pool = require("../models/pool");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 router.post("/sign-up", async (req, res) => {
   try {
     const { username, password, email } = req.body;
     const created_on = new Date();
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const text =
       "INSERT INTO account (username, password, email, created_on) VALUES ($1, $2, $3, $4) RETURNING *";
-    const values = [username, password, email, created_on];
+    const values = [username, hashedPassword, email, created_on];
 
     const { rows } = await pool.query(text, values);
 
-    res.status(200).json(rows);
+    res.status(200).json(rows[0]);
   } catch (error) {
-    console.log(error);
-    res.status(400);
+    res.status(400).json({ error });
   }
 });
 
@@ -24,15 +26,24 @@ router.post("/sign-in", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const text = "SELECT * FROM account WHERE username = $1 AND password = $2";
-    const values = [username, password];
+    const text = "SELECT * FROM account WHERE username = $1";
+    const values = [username];
 
     const { rows } = await pool.query(text, values);
 
-    res.status(200).json(rows);
+    let pass = false;
+
+    if (rows.length !== 0) {
+      pass = await bcrypt.compare(password, rows[0].password);
+    }
+
+    if (!pass) {
+      return res.status(401).json({ error: "invalid username or password" });
+    }
+
+    res.status(200).json(rows[0]);
   } catch (error) {
-    console.log(error);
-    res.status(400);
+    res.status(400).json({ error });
   }
 });
 
