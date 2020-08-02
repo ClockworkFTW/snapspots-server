27f914bc648f647841c3c1e66800cbc067349cd1
John Boyle <nikolasboyle@gmail.com>
2020-08-02 14:40:45 -0700

    Add spot creation and voting

--- routes/spots.js
+++ routes/spots.js
@@ -1,9 +1,32 @@
 const router = require("express").Router();
 
+const pool = require("../models/pool");
+
 const google = require("../services/google");
 const flickr = require("../services/flickr");
 
-router.get("/:place_id", async (req, res) => {
+const getSpot = async (id) => {
+  const spot = await pool.query("SELECT * FROM spots WHERE spot_id = $1", [id]);
+  const votes = await pool.query("SELECT * FROM votes WHERE spot_id = $1", [
+    id,
+  ]);
+
+  return { ...spot.rows[0], votes: votes.rows };
+};
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const spot = await getSpot(id);
+
+    res.status(200).json(spot);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.get("/all/:place_id", async (req, res) => {
   try {
     const location = await google.geocode(req.params.place_id);
 
@@ -50,8 +73,46 @@ router.get("/:place_id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).end();
+    const {
+      name,
+      description,
+      keywords,
+      coords,
+      type,
+      equipment,
+      time,
+      photos,
+      user,
+    } = req.body;
+
+    const text =
+      "INSERT INTO spots (name, description, type, equipment, time, photos, account_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
+    const values = [name, description, type, equipment, time, photos, user];
+
+    const { rows } = await pool.query(text, values);
+
+    res.status(200).json({ ...rows[0], votes: [] });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.post("/vote", async (req, res) => {
+  try {
+    const { vote_id, spot_id, account_id, up } = req.body;
+
+    if (vote_id) {
+      await pool.query("DELETE FROM votes WHERE vote_id=($1)", [vote_id]);
+    } else {
+      await pool.query(
+        "INSERT INTO votes (spot_id, account_id, up) VALUES ($1, $2, $3)",
+        [spot_id, account_id, up]
+      );
+    }
+
+    const spot = await getSpot(spot_id);
+
+    res.status(200).json(spot);
   } catch (error) {
     res.status(400).json({ error });
   }
