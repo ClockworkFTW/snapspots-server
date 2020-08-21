4aa816b258c6516f97ec7a29f7dab7c44944d6f2
John Boyle <nikolasboyle@gmail.com>
2020-08-21 11:41:48 -0700

    Fix pg pool connection

--- models/pool.js
+++ models/pool.js
@@ -1,10 +1,20 @@
 const { Pool } = require("pg");
 
+// Desktop
 const pool = new Pool({
-  host: "localhost",
-  user: "johnboyle",
+  host: "127.0.0.1",
+  user: "postgres",
+  password: "#Trinity13",
   port: 5432,
   database: "snapspots",
 });
+
+// Macbook
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "johnboyle",
+//   port: 5432,
+//   database: "snapspots",
+// });
 
 module.exports = pool;
